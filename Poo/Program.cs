11ddc96e0b838e742c917427d4827d5180a034cc
Copil Alina <copilalinarodica17@gmail.com>
2025-05-07 15:52:49 +0300

    Class Program

--- Poo/Program.cs
+++ Poo/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿List<Student> student = new List<Student>();
+Student s1 = new Student("Copil", "Alina", 010, 3);
+Student s2 = new Student("Ciuculescu", "Denisa", 111, 3);
+Disciplina d1 = new Disciplina("Mate", 123);
+Disciplina d2 = new Disciplina("Romana", 235);
+s1.AddMaterii(d1);
+s2.AddMaterii(d2);
+s1.PrintMaterii();
+s2.PrintMaterii();
