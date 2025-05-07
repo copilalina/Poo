3ca1babd296f0f23f4a6cbc2bf93d747d949dcb7
Copil Alina <copilalinarodica17@gmail.com>
2025-05-07 15:44:15 +0300

    Class Disciplina

---
+++ Poo/Disciplina.cs
@@ -0,0 +1,12 @@
+﻿using System.Xml.Schema;
+
+public class Disciplina
+{
+    public string Name { get; set; }
+    public int Cod {  get; set; }
+    public Disciplina(string name,int cod)
+    {
+        Name = name;
+        Cod = cod;
+    }
+}
