638e35514a773f9490bc27ce9a40fc23e509deda
Copil Alina <copilalinarodica17@gmail.com>
2025-05-07 15:49:07 +0300

    Class Student

---
+++ Poo/Student.cs
@@ -0,0 +1,26 @@
+﻿public class Student
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int RegistrationNumber { get; set; }
+    public int AnStudiu {  get; set; }
+    List<Disciplina>Materii=new List<Disciplina>();
+
+    public Student(string firstName, string lastName,int registrationNumber,int anstudiu)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        RegistrationNumber = registrationNumber;
+        AnStudiu = anstudiu;
+    }
+    public void AddMaterii(Disciplina materii)
+    {
+        Materii.Add(materii);
+    }
+    public void PrintMaterii() { 
+    foreach(var materii in Materii)
+        {
+            Console.WriteLine(materii.Name);
+        }
+    }
+}
